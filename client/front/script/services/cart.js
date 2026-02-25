80667f659920edff8ba3256f6ae56f67e6ff5124
Victor <victoreduardomartins2@gmail.com>
2026-02-24 22:03:41 -0300

    commiting alterations

--- client/front/script/services/cart.js
+++ client/front/script/services/cart.js
@@ -2,9 +2,15 @@
 import { API_CLIENT_KEY, API_CART, getUserFromToken, fetchAuth } from "./config"
 
 async function addToCart(id_usuario, id_produto_variacao, quantidade) {
+    
     try {
         const res = await fetchAuth(API_CART, {
-            method: // fazer fetch do carrinho
+            method:"POST",
+            headers:{
+                "Content-Type": "application/json",
+                "shift-api-key": API_CLIENT_KEY
+            },
+            body: JSON.parse(id_usuario, id_produto_variacao, quantidade)
         })
     } catch (err) {
         console.error(err.message);
